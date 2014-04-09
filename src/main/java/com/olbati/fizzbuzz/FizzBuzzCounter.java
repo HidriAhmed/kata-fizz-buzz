5053f2b6f2b09cfc57ecb6d613976b8298ea068c
Ahmed Hidri <hidri.ahmd@gmail.com>
2014-04-09 14:45:17 +0200

    commit Implementation of the class with refactoring of the method count

--- src/main/java/com/olbati/fizzbuzz/FizzBuzzCounter.java
+++ src/main/java/com/olbati/fizzbuzz/FizzBuzzCounter.java
@@ -5,11 +5,15 @@ public class FizzBuzzCounter {
 	public String[] count() {
 		String[] result = new String[100];
 
-		result[1] = "1";
-		result[2] = "2";
-		result[3] = "Fizz";
-		result[4] = "4";
-		result[5] = "Buzz";
+		for (Integer i = 0; i <= 100; i++) {
+			if (i == 3) {
+				result[i] = "Fizz";
+			} else if (i == 5) {
+				result[i] = "Buzz";
+			} else {
+				result[i] = i.toString();
+			}
+		}
 
 		return result;
 	}
