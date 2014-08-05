f8f135a95564a0d07520bf5b6f8329f57451ff0b
Ahmed Hidri <hidri.ahmd@gmail.com>
2014-08-05 23:31:18 +0200

    Add multiple of 3 and 5 counter cases Test

--- src/main/java/com/olbati/fizzbuzz/FizzBuzzCounterStage1.java
+++ src/main/java/com/olbati/fizzbuzz/FizzBuzzCounterStage1.java
@@ -1,23 +1,25 @@
 package com.olbati.fizzbuzz;
 
-public class FizzBuzzCounter {
+public class FizzBuzzCounterStage1 {
 
-	public String[] count() {
-		String[] result = new String[101];
+    public String[] count() {
+	String[] result = new String[101];
 
-		for (Integer i = 0; i <= 100; i++) {
-			if (((i % 3) == 0) && ((i % 5) == 0)) {
+	for (Integer i = 0; i <= 100; i++) {
+	    if (((i % 3) == 0) && ((i % 5) == 0)) {
+		result[i] = "FizzBuzz";
+	    } else {
+		if ((i % 3) == 0) {
 
-			}
-			if ((i % 3) == 0) {
-				result[i] = "Fizz";
-			} else if ((i % 5) == 0) {
-				result[i] = "Buzz";
-			} else {
-				result[i] = i.toString();
-			}
+		    result[i] = "Fizz";
+		} else if ((i % 5) == 0) {
+		    result[i] = "Buzz";
+		} else {
+		    result[i] = i.toString();
 		}
-
-		return result;
+	    }
 	}
+
+	return result;
+    }
 }
--- src/test/java/com/olbati/fizzbuzz/TestFizzBuzzCounterStage1.java
+++ src/test/java/com/olbati/fizzbuzz/TestFizzBuzzCounterStage1.java
@@ -4,96 +4,96 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
 
-public class TestFizzBuzzCounter {
+public class TestFizzBuzzCounterStage1 {
 
-	@Test
-	public void givenIndex1_shouldReturn1() {
+    @Test
+    public void givenIndex1_shouldReturn1() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[1]).isEqualTo("1");
+	assertThat(result[1]).isEqualTo("1");
 
-	}
+    }
 
-	@Test
-	public void givenIndex2_shouldReturn2() {
+    @Test
+    public void givenIndex2_shouldReturn2() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[2]).isEqualTo("2");
+	assertThat(result[2]).isEqualTo("2");
 
-	}
+    }
 
-	@Test
-	public void givenIndex3_shouldReturnFizz() {
+    @Test
+    public void givenIndex3_shouldReturnFizz() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[3]).isEqualTo("Fizz");
+	assertThat(result[3]).isEqualTo("Fizz");
 
-	}
+    }
 
-	@Test
-	public void givenIndex4_shouldReturn4() {
+    @Test
+    public void givenIndex4_shouldReturn4() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[4]).isEqualTo("4");
+	assertThat(result[4]).isEqualTo("4");
 
-	}
+    }
 
-	@Test
-	public void givenIndex5_shouldReturnBuzz() {
+    @Test
+    public void givenIndex5_shouldReturnBuzz() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[5]).isEqualTo("Buzz");
+	assertThat(result[5]).isEqualTo("Buzz");
 
-	}
+    }
 
-	@Test
-	public void givenIndex32_shouldReturn32() {
+    @Test
+    public void givenIndex32_shouldReturn32() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[32]).isEqualTo("32");
+	assertThat(result[32]).isEqualTo("32");
 
-	}
+    }
 
-	@Test
-	public void givenIndex10_shouldReturnBuzz() {
+    @Test
+    public void givenIndex10_shouldReturnBuzz() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[10]).isEqualTo("Buzz");
+	assertThat(result[10]).isEqualTo("Buzz");
 
-	}
+    }
 
-	@Test
-	public void givenIndex6_shouldReturnFizz() {
+    @Test
+    public void givenIndex6_shouldReturnFizz() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[6]).isEqualTo("Fizz");
+	assertThat(result[6]).isEqualTo("Fizz");
 
-	}
+    }
 
-	@Test
-	public void givenIndex15_shouldReturnFizzBuzz() {
+    @Test
+    public void givenIndex15_shouldReturnFizzBuzz() {
 
-		FizzBuzzCounter counter = new FizzBuzzCounter();
-		String[] result = counter.count();
+	FizzBuzzCounterStage1 counter = new FizzBuzzCounterStage1();
+	String[] result = counter.count();
 
-		assertThat(result[6]).isEqualTo("Fizz");
+	assertThat(result[15]).isEqualTo("FizzBuzz");
 
-	}
+    }
 
 }
